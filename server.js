d263f03b49080446860e4f18a125051d709ad3ff
tarek1710 <sharifuj007@gmail.com>
2026-04-25 23:07:27 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+http.listen(3000, () => {
+    console.log("Running on 3000");
+});
